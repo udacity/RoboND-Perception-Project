f78644f987397d0d4b264d94d802de597a2a3d6a
robond <hvpandya@ncsu.edu>
2017-07-24 18:57:50 -0700

    Initial commit

---
+++ pr2_robot/scripts/project_template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# TODO: import modules
+
+def pr2_mover(): 
+
+    # TODO: Read "/object_list" and "/dropbox" parameters
+
+    # TODO: Parse the parameters
+
+    # TODO: Recognize objects from the pick_list
+
+    # TODO: Create ROS Client for "pick_place_routine" service
+
+    # TODO: Place Service Request for each item in the pick_list 
+
+
+if __name__ == '__main__':
+    try:
+        pr2_mover()
+    except rospy.ROSInterruptException:
+        pass
