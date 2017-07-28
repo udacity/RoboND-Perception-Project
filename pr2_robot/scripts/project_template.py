dff9b64e6c629e8e5dbd3e7cd2f5b535d18f81d1
harsh <hvpandya@ncsu.edu>
2017-07-28 15:36:51 -0700

    Added more instructions to the solution template

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -1,22 +1,125 @@
 #!/usr/bin/env python
 
-# TODO: import modules
+# Import modules
+import rospy
+import numpy as np
+import sklearn
+from sklearn.preprocessing import LabelEncoder
 
-def pr2_mover(): 
+import pickle
 
-    # TODO: Read "/object_list" and "/dropbox" parameters
+from sensor_stick.srv import GetNormals
+from sensor_stick.features import compute_color_histograms
+from sensor_stick.features import compute_normal_histograms
+from visualization_msgs.msg import Marker
 
-    # TODO: Parse the parameters
+from sensor_stick.marker_tools import *
+from sensor_stick.msg import DetectedObjectsArray
+from sensor_stick.msg import DetectedObject
 
-    # TODO: Recognize objects from the pick_list
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Int32
+from std_msgs.msg import String
+from pr2_robot.srv import *
+from pcl_helper import *
 
-    # TODO: Create ROS Client for "pick_place_routine" service
+def get_normals(cloud):
+    get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
+    return get_normals_prox(cloud).cluster
 
-    # TODO: Place Service Request for each item in the pick_list 
+# Callback function for your Point Cloud Subscriber
+def pcl_callback(pcl_msg):
+
+# Exercise-2 TODOs:
+
+    # TODO: Convert ROS msg to PCL data
+
+    # TODO: Voxel Grid Downsampling
+
+    # TODO: PassThrough Filter
+
+    # TODO: RANSAC Plane Segmentation
+
+    # TODO: Extract inliers and outliers
+
+    # TODO: Euclidean Clustering
+
+    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+
+    # TODO: Convert PCL data to ROS messages
+
+    # TODO: Publish ROS messages
+
+# Exercise-3 TODOs:
+
+    # Classify the clusters! (loop through each detected cluster one at a time)
+
+        # Grab the points for the cluster
+
+        # Compute the associated feature vector
+
+        # Make the prediction
+
+        # Publish a label into RViz
+
+        # Add the detected object to the list of detected objects.
+
+    # Publish the list of detected objects
+
+
+# function to load parameters and request PickPlace service
+def pr2_mover():
+
+    # TODO: Initialize variables
+
+    # TODO: Get/Read parameters
+
+    # TODO: Parse parameters into individual variables
+
+    # TODO: Rotate PR2 in place to capture side tables for the collision map
+
+    # TODO: Loop through the pick list
+
+        # TODO: Get the PointCloud for a given object and obtain it's centroid
+
+        # TODO: Create 'place_pose' for the object
+
+        # TODO: Assign the arm to be used for pick_place
+
+        # Wait for 'pick_place_routine' service to come up
+        rospy.wait_for_service('pick_place_routine')
+
+        try:
+            pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
+
+            # TODO: Insert your variables to be sent as a service request
+            resp = pick_place_routine(TEST_SCENE_NUM, OBJECT_NAME, WHICH_ARM, PICK_POSE, PLACE_POSE)
+
+            print ("Response: ",resp.success)
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
+        # TODO: Output your request parameters into output yaml file
+
 
 
 if __name__ == '__main__':
+
+    # TODO: ROS node initialization
+
+    # TODO: Create Subscribers
+
+    # TODO: Create Publishers
+
+    # TODO: Load Model From disk
+
+    # Initialize color_list
+    get_color_list.color_list = []
+
     try:
         pr2_mover()
     except rospy.ROSInterruptException:
-pass
+        pass
+
+    # TODO: Spin while node is not shutdown
