bf755e6bfabcc552f486319369c05b5fb9f3fedd
rykeenan <ryan.keenan@udacity.com>
2017-07-30 22:20:25 -0700

    update project_template.py

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -1,31 +1,50 @@
 #!/usr/bin/env python
 
 # Import modules
-import rospy
 import numpy as np
 import sklearn
 from sklearn.preprocessing import LabelEncoder
-
 import pickle
-
 from sensor_stick.srv import GetNormals
 from sensor_stick.features import compute_color_histograms
 from sensor_stick.features import compute_normal_histograms
 from visualization_msgs.msg import Marker
-
 from sensor_stick.marker_tools import *
 from sensor_stick.msg import DetectedObjectsArray
 from sensor_stick.msg import DetectedObject
+from sensor_stick.pcl_helper import *
 
+import rospy
+import tf
 from geometry_msgs.msg import Pose
+from std_msgs.msg import Float64
 from std_msgs.msg import Int32
 from std_msgs.msg import String
 from pr2_robot.srv import *
-from pcl_helper import *
+from rospy_message_converter import message_converter
+import yaml
 
+
+# Helper function to get surface normals
 def get_normals(cloud):
     get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
     return get_normals_prox(cloud).cluster
+
+# Helper function to create a yaml friendly dictionary from ROS messages
+def make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose):
+    yaml_dict = {}
+    yaml_dict["test_scene_num"] = test_scene_num.data
+    yaml_dict["arm_name"]  = arm_name.data
+    yaml_dict["object_name"] = object_name.data
+    yaml_dict["pick_pose"] = message_converter.convert_ros_message_to_dictionary(pick_pose)
+    yaml_dict["place_pose"] = message_converter.convert_ros_message_to_dictionary(place_pose)
+    return yaml_dict
+
+# Helper function to output to yaml file
+def send_to_yaml(yaml_filename, dict_list):
+    data_dict = {"object_list": dict_list}
+    with open(yaml_filename, 'w') as outfile:
+        yaml.dump(data_dict, outfile, default_flow_style=False)
 
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
@@ -66,9 +85,16 @@ def pcl_callback(pcl_msg):
 
     # Publish the list of detected objects
 
+    # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
+    # Could add some logic to determine whether or not your object detections are robust
+    # before calling pr2_mover()
+    try:
+        pr2_mover(detected_objects_list)
+    except rospy.ROSInterruptException:
+        pass
 
 # function to load parameters and request PickPlace service
-def pr2_mover():
+def pr2_mover(object_list):
 
     # TODO: Initialize variables
 
@@ -86,13 +112,15 @@ def pr2_mover():
 
         # TODO: Assign the arm to be used for pick_place
 
+        # TODO: Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
+
         # Wait for 'pick_place_routine' service to come up
         rospy.wait_for_service('pick_place_routine')
 
         try:
             pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
 
-            # TODO: Insert your variables to be sent as a service request
+            # TODO: Insert your message variables to be sent as a service request
             resp = pick_place_routine(TEST_SCENE_NUM, OBJECT_NAME, WHICH_ARM, PICK_POSE, PLACE_POSE)
 
             print ("Response: ",resp.success)
@@ -100,7 +128,7 @@ def pr2_mover():
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
 
-        # TODO: Output your request parameters into output yaml file
+    # TODO: Output your request parameters into output yaml file
 
 
 
@@ -116,10 +144,5 @@ if __name__ == '__main__':
 
     # Initialize color_list
     get_color_list.color_list = []
-
-    try:
-        pr2_mover()
-    except rospy.ROSInterruptException:
-        pass
 
     # TODO: Spin while node is not shutdown
