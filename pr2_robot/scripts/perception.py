2285e00c8f9326f06476c009f212f41a86097923
JUNG Myoungki <myoungki.jung@gmail.com>
2017-08-03 00:09:42 +1000

    Feat: Object population
     && Todo: capture training svm and make model.sav
     &$  place_pose,pick_pose type matching
     &$ parsing xml file for test_scene_num

--- pr2_robot/scripts/perception.py
+++ pr2_robot/scripts/perception.py
@@ -23,7 +23,7 @@ from std_msgs.msg import String
 from pr2_robot.srv import *
 from rospy_message_converter import message_converter
 import yaml
-
+import re
 
 # Helper function to get surface normals
 def get_normals(cloud):
@@ -84,8 +84,8 @@ def pcl_callback(pcl_msg):
     tree = white_cloud.make_kdtree()
     ec = white_cloud.make_EuclideanClusterExtraction()
     ec.set_ClusterTolerance(0.0385)
-    ec.set_MinClusterSize(20)
-    ec.set_MaxClusterSize(200)
+    ec.set_MinClusterSize(30)
+    ec.set_MaxClusterSize(800)
     ec.set_SearchMethod(tree)
     cluster_indices = ec.Extract()
 
@@ -168,24 +168,59 @@ def pcl_callback(pcl_msg):
 # function to load parameters and request PickPlace service
 def pr2_mover(object_list):
 
-    # TODO: Initialize variables
-
-    # TODO: Get/Read parameters
+    # Initialize variables
+    object_list_param = []
+    dropbox_param = []
+    # Get/Read parameters
+    object_list_param = rospy.get_param('/object_list')
+    dropbox_param = rospy.get_param('/dropbox')
 
     # TODO: Parse parameters into individual variables
-
+    test_scene_num = Int32()
+    test_scene_num.data = 1
+    #regex to find the world name and extract it
+    # if len(object_list_param) == 8:
+    #     test_scene_num = 3
+    # elif len(object_list_param) == 5:
+    #     test_scene_num = 2
+    # elif len(object_list_param) == 3:
+    #     test_scene_num = 1
+    # else:
+    #     test_scene_num = 0
+    object_name = String()
+    arm_name = String()
+    pick_pose = Pose()
+    place_pose = Pose()
     # TODO: Rotate PR2 in place to capture side tables for the collision map
 
-    # TODO: Loop through the pick list
+    yaml_dict_list = []
+    labels = []
+    pick_centroids = [] # to be detected object list of tuples (x, y, z)
+    place_centroids = [] # to be placing bin list of tuples (x, y, z)
 
-        # TODO: Get the PointCloud for a given object and obtain it's centroid
+    # TODO: Loop through the pick list
+    for index, object in enumerate(object_list):
+
+        object_name.data = object_list_param[index]['name']
+        object_group = object_list_param[index]['group']
+
+        # Get the PointCloud for a given object and obtain it's centroid
+        labels.append(object.label)
+        points_arr = ros_to_pcl(object.cloud).to_array()
+        pick_pose.position = np.mean(points_arr, axis=0)[:3]
+        pick_centroids.append(pick_pose)
 
         # TODO: Create 'place_pose' for the object
-
+        for i, dropbox in enumerate(dropbox_param):
+            if object_group == dropbox_param[i]["group"]:
+                place_pose.position = dropbox_param[i]["position"]
+                place_centroids.append(place_pose)
         # TODO: Assign the arm to be used for pick_place
+                arm_name.data = dropbox_param[i]["name"]
 
-        # TODO: Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
-
+        # Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
+        yaml_dict = make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose)
+        yaml_dict_list.append(yaml_dict)
         # Wait for 'pick_place_routine' service to come up
         rospy.wait_for_service('pick_place_routine')
 
@@ -193,27 +228,31 @@ def pr2_mover(object_list):
             pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
 
             # TODO: Insert your message variables to be sent as a service request
-            resp = pick_place_routine(TEST_SCENE_NUM, OBJECT_NAME, WHICH_ARM, PICK_POSE, PLACE_POSE)
+            
+            resp = pick_place_routine(test_scene_num, object_name, arm_name, pick_pose, place_pose)
 
             print ("Response: ",resp.success)
 
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
 
-    # TODO: Output your request parameters into output yaml file
-
+    # Output your request parameters into output yaml file
+    yaml_filename = "yamnl_output"
+    #TODO: make it to reflect the test case
+    send_to_yaml(yaml_filename, yaml_dict_list)
 
 
 if __name__ == '__main__':
+    # TODO: Read the pick_place_project.launch xml file and regex line 13 <arg name="world_name" value="$(find pr2_robot)/worlds/test1.world"/>
 
-    # TODO: ROS node initialization
+    # ROS node initialization
     rospy.init_node('clustering', anonymous=True)
 
-    # TODO: Create Subscribers
+    # Create Subscribers
     #pcl_sub = rospy.Subscriber("/sensor_stick/point_cloud", pc2.PointCloud2, pcl_callback, queue_size=1)
     pcl_sub = rospy.Subscriber("/pr2/world/points", pc2.PointCloud2, pcl_callback, queue_size=1)
 
-    # TODO: Create Publishers
+    # Create Publishers
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
     pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
     pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
@@ -221,7 +260,7 @@ if __name__ == '__main__':
     object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=5)
     detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
     
-    # TODO: Load Model From disk
+    #  Load Model From disk
     model = pickle.load(open('model.sav', 'rb'))
     clf = model['classifier']
     encoder = LabelEncoder()
@@ -231,6 +270,6 @@ if __name__ == '__main__':
     # Initialize color_list
     get_color_list.color_list = []
 
-    # TODO: Spin while node is not shutdown
+    # Spin while node is not shutdown
     while not rospy.is_shutdown():
         rospy.spin()
